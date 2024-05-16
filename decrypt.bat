b31d033a17d097b9fa11c1d94f894bbc5a923bbb
zhangnd <920563959@qq.com>
2024-05-16 08:44:02 +0800

    Update decrypt.bat

--- decrypt.bat
+++ decrypt.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 set /a index=0
 for /r tmp %1 %%i in (*) do (
     set /a index+=1
-    openssl aes-128-cbc -d -in %%i -out tmp/!index!.ts -nosalt -K 0b53fbec72cb3a6d4d9c8efbc23a025e -iv 00000000000000000000000000000000
+    openssl aes-128-cbc -d -in %%i -out tmp/!index!.ts -nosalt -K c7719993cb5b81ceb148f4a205d48f05 -iv 00000000000000000000000000000000
     echo file 'tmp/!index!.ts'>> input.txt
 )
 endlocal
